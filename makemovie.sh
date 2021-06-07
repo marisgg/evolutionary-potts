3d6a069c94c3bc54a066973ee6ed85b9527fe2a7
Yannick Hogewind <yannickhogewind@gmail.com>
2021-06-07 17:21:19 +0200

    Make makemovie more robust

--- makemovie.sh
+++ makemovie.sh
@@ -1,2 +1,8 @@
-for i in output/img/ForagingModel/*.png;do convert $i -background white -alpha remove -alpha off -set filename:base "%[base]" "output/img/ForagingModel/%[filename:base].jpg";done
+for i in output/img/ForagingModel/*.png
+do
+    PNGNAME=$i
+    echo $PNGNAME
+    JPGNAME=$(echo $PNGNAME | sed -r 's/.png/.jpg/g')
+    convert $PNGNAME -background white -alpha remove -alpha off $JPGNAME
+done
 ffmpeg -y -r 30 -f image2 -i output/img/ForagingModel/ForagingModel-t%d0.jpg -vcodec libx264 -crf 25 -pix_fmt yuv420p output/mp4/ForagingModel.mp4
