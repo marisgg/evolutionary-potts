4576027efbf3c31f4559c93d78b4ebd192e957de
Yannick Hogewind <yannickhogewind@gmail.com>
2021-06-09 13:34:34 +0200

    Finalize experimental method

--- evolution/evolution.py
+++ evolution/evolution.py
@@ -9,11 +9,12 @@ import json
 import numpy as np
 
 PARAM_DIR = "./params"
-GENERATION_SIZE = 4*12
+GENERATION_SIZE = 6
 MUTATION_SCALE = .5
 N_ELITE = 2
 POTTS_SEED = 1
 CHANGE_POTTS_SEED_PER_GEN = True
+GENERATION_NO = 1
 np.random.seed(POTTS_SEED)
 
 def mutate_cell(cell):
@@ -46,22 +47,26 @@ def fitness_from_tuple(output):
             return float(s)
     spl = output.rstrip("\n").split(",")
     int_tuple = tuple(map(to_int_or_float, spl))
-    # fitness = time alive + livelihood left + (100 - distance to nearest food)
-    fitness = int_tuple[0]
-    return int_tuple[0]+ int_tuple[1] + (100+int_tuple[2])
+    # fitness = time alive-minimum lifetime + livelihood left + (200 - distance to nearest food)
+    # fitness = int_tuple[0]
+    return int_tuple[0]+ int_tuple[1] + (200+int_tuple[2])
 
 def run_js_simulation(args):
     (modelname, paramname) = args
     cmd = f"node ./{modelname} {paramname}"
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    output = proc.communicate()[0]
-    output = output.decode("utf-8")
-    return output
+    try:
+        output = proc.communicate()[0]
+        output = output.decode("utf-8")
+        return fitness_from_tuple(output)
+    except:
+        # failed
+        return 0
     # outputIO = io.StringIO(output)
     # df = pd.read_csv(outputIO, sep="\t", header=None, names=["step","id","type","x","y"])
     # return df
 
-def create_param_files(generation):
+def create_param_files(generation, prefix="", seed=None):
     j = json.loads('''
                     {
                         "conf":{
@@ -77,10 +82,14 @@ def create_param_files(generation):
                     }''')
     paramnames = []
     global POTTS_SEED
-    j["conf"]["seed"] = POTTS_SEED
-    print(POTTS_SEED)
-    if CHANGE_POTTS_SEED_PER_GEN:
-        POTTS_SEED = POTTS_SEED + 1
+    if seed is None:
+        j["conf"]["seed"] = POTTS_SEED
+        seed = POTTS_SEED
+    else:
+        j["conf"]["seed"] = seed
+    print("seed:", POTTS_SEED, "previous seed for run 2:", seed)
+    directory  =f"{PARAM_DIR}/gen{GENERATION_NO}/"
+    os.makedirs(directory, exist_ok=True)
     for i, cell in enumerate(generation):
         j["conf"]["MAX_ACT"][-1] = cell["MAX_ACT"]
         j["conf"]["V"][-1] = cell["V"]
@@ -89,18 +98,29 @@ def create_param_files(generation):
         j["conf"]["LAMBDA_V"][-1] = cell["LAMBDA_V"]
         j["conf"]["LAMBDA_P"][-1] = cell["LAMBDA_P"]
         j["conf"]["LAMBDA_CH"][-1] = cell["LAMBDA_CH"]
-        filename = f"{PARAM_DIR}/{i}.json"
+        filename = f"{directory}/{prefix}{i}.json"
         paramnames.append(filename)
         with open(filename, "w+") as f:
             f.write(json.dumps(j))
     return paramnames
 
+def simulate_two(obj):
+    # obj: (modelname, (param1, param2))
+    (modelname, (param1, param2)) = obj
+    return (run_js_simulation((modelname, param1))+run_js_simulation((modelname, param2)))/2
+
 def simulate_generation(generation, modelname, num_procs=12):
+    global POTTS_SEED
     paramnames = create_param_files(generation)
-    args = list(map(lambda x: (modelname, x), paramnames))
+    # Also run with previous seed to smooth out errors
+    paramnames2 = create_param_files(generation, "seed2_", POTTS_SEED-1)
+    if CHANGE_POTTS_SEED_PER_GEN:
+        POTTS_SEED = POTTS_SEED + 1
+    paramnametuple = zip(paramnames, paramnames2)
+    args = list(map(lambda tup: (modelname, tup), paramnametuple))
     with Pool(num_procs) as p:
-        sim_results = p.map(run_js_simulation, args)
-    fitnesses = map(fitness_from_tuple, sim_results)
+        sim_results = p.map(simulate_two, args)
+    fitnesses = sim_results
     gen_fitnesses = list(zip(generation, fitnesses))
     return gen_fitnesses
 
@@ -123,9 +143,10 @@ def next_gen_elites_only(generation_with_fitnesses):
 def next_generation_elitism_and_roulette(generation_with_fitnesses):
     # Sort by increasing fitness
     gen_w_f = sorted(generation_with_fitnesses, key=lambda x: x[1], reverse=True)
+
     print(gen_w_f)
     fitnesses = list(map(lambda x: x[1], gen_w_f))
-
+    print(f"Genfitness min: ", np.min(fitnesses), " mean: ", np.mean(fitnesses), " median: ", np.median(fitnesses), " max: ", np.max(fitnesses))
     gen_w_f = list(map(lambda x: x[0], gen_w_f))
     elites = gen_w_f[:N_ELITE]
 
@@ -134,7 +155,7 @@ def next_generation_elitism_and_roulette(generation_with_fitnesses):
     #     gen_w_f.append(mutate_cell(gen_w_f[i%N_ELITE]))
     #     i += 1
     sample_weights = np.array(fitnesses)
-    sample_weights = sample_weights - np.min(sample_weights) + 10
+    sample_weights = sample_weights - np.min(sample_weights) + 50
     sample_weights = sample_weights/sum(sample_weights)
     print(sample_weights)
     sampled_cells = np.random.choice(gen_w_f, size=GENERATION_SIZE-N_ELITE, p=sample_weights)
@@ -164,10 +185,18 @@ def init_individual():
         start[p] = np.round(np.random.uniform(low=2, high=50), decimals=2)
     return start
 
-def evolve(filename, num_generations):
+def evolve(filename, num_generations, seed=None):
+    global POTTS_SEED
+    if seed is not None:
+        POTTS_SEED = int(seed)
+    print(f"Starting to simulate for {filename}, {num_generations}")
+    print(f"Starting seed: {POTTS_SEED}")
     init()
     generation = [init_individual() for i in range(GENERATION_SIZE)]
     #generation = list(map(mutate_cell, generation))
     for i in range(num_generations):
+        global GENERATION_NO
+        print(f"Simulation generation: {GENERATION_NO}")
         gen_fitnesses = simulate_generation(generation, filename, num_procs=cpu_count()-1)
+        GENERATION_NO = GENERATION_NO+1
         generation = next_generation_elitism_and_roulette(gen_fitnesses)
--- evolution/run.py
+++ evolution/run.py
@@ -5,4 +5,8 @@ A = "src/runActModel.js"
 
 # This main is needed on Windows to prevent recursive subprocesses opening
 if __name__ in '__main__':
-    evolution.evolve(F, 30)
+    seed = None
+    if seed is None:
+        print("Enter a starting seed:")
+        seed = int(input())
+    evolution.evolve(F, 30, seed)
